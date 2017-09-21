9d95aa6630b34effe89d1729695f2f434417e646
IsabellaOInsper <isabellaro@al.insper.edu.br>
2017-09-20 22:41:22 -0300

    Salvando os 11 audios na pasta 'arquivos'

--- src/decoderDTMF.py
+++ src/decoderDTMF.py
@@ -8,6 +8,7 @@ Created on Wed Sep 20 13:11:43 2017
 import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
+import soundfile as sf # Para instalar ele: https://pysoundfile.readthedocs.io/en/0.9.0/
 
 t = 1
 fs = 44100
@@ -29,12 +30,21 @@ def Play(y, fs):
     # aguarda fim da reprodução
     sd.wait()
     print("Fim da reprodução")
-    
-y = Record(duration, fs)
-Play(y, fs)
 
-plt.xlabel('Tempo')
-plt.ylabel('Valor')
-plt.plot(lista_tempo[0:1000], y[0:1000], color="#FF00D4")
-plt.show()
+contador = 0
+while contador <= 11:
+    y = Record(duration, fs)
+    Play(y, fs)
+    
+    plt.xlabel('Tempo')
+    plt.ylabel('Valor')
+    plt.plot(lista_tempo[0:1000], y[0:1000], color="#FF00D4")
+    plt.show()
+    
+    audio = "./arquivos/audio"
+    audio = audio + str(contador) + ".wav"
+    sf.write(audio, y, fs)
+    contador += 1
+    
+    
     
